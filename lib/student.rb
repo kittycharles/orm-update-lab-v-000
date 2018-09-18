7179e173c166de098a44f4bd3771426dc5ebb315
Richard Sanchez <richard.charles.s@gmail.com>
2018-09-18 16:54:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,8 +53,7 @@ class Student
   end
 
   def self.new_from_db(row)
-      student = Student.new(row[0], row[1], row[2])
-      binding.pry
+      student = Student.new(row[1], row[2], row[0])
       student
     end
 
