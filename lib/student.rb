b533cf870d6c0c986588dddbab941048acd4e577
Richard Sanchez <richard.charles.s@gmail.com>
2018-09-18 16:38:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,6 +27,9 @@ class Student
   end
 
   def save
+    if self.id
+      self.update
+    else
     sql = <<-SQL
       INSERT INTO students (name, grade)
       VALUES (?, ?)
@@ -35,6 +38,7 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
+  end
 
-  
+
 end
