6214723f06328fe2114770607035992be6de8943
Richard Sanchez <richard.charles.s@gmail.com>
2018-09-18 16:49:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,7 +52,7 @@ class Student
   end
 
   def self.new_from_db(row)
-    new_student = self.new(row[0], row[1], row[2])
+    new_student = Student.new(row[0], row[1], row[2])
     new_student
   end
 
